77b7587d52d41ea309c17c0446d16f363e6acd02
jjanicechen <jjanicechen2013@gmail.com>
2017-06-17 23:44:25 -0700

    Rename interface as client

--- lib/zeus/api_client.rb
+++ lib/zeus/api_client.rb
@@ -12,20 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'zeus/api_client/alerts_interface'
-require 'zeus/api_client/metrics_interface'
-require 'zeus/api_client/logs_interface'
-require 'zeus/api_client/trigalerts_interface'
+require 'zeus/api_client/alerts_client'
+require 'zeus/api_client/metrics_client'
+require 'zeus/api_client/logs_client'
+require 'zeus/api_client/trigalerts_client'
 
 module Zeus
   # API Client for Zeus Service
   class APIClient
-    include AlertsInterface, MetricsInterface,
-            LogsInterface, TrigalertsInterface
+    include AlertsClient, MetricsClient,
+            LogsClient, TrigalertsClient
 
     # constructor for Zeus::APIClient
     # @param       [Hash] opts the options to create z Zeus::APIClient instance
-    # @option opts [String] :token The tokens (user token or access token) for Zeus Service
+    # @option opts [String] :token The tokens (user token/access token) for Zeus Service
     # @option opts [String] :endpoint The base url for API endpoint
     def initialize(opts = {})
       @token = opts[:token]
